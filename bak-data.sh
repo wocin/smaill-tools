6bfda06175d9ce906a4592e5c5cadc4260e651ec
wocin <925511765@qq.com>
2013-12-24 17:21:14 +0800

    Update bak-data.sh

--- bak-data.sh
+++ bak-data.sh
@@ -3,8 +3,16 @@
 #Email ---
 #-------------------I am boring line------------------------------------
 #bakup mysql
-database=`mysql -u root -p"" -e "show databases"|grep -v "information_schema"|grep -v "mysql"|grep -v "test"|grep -v "Database"`
-for i in $database;do
-    mysqldump -u root -p"" $i>/backup/$i.sql;
+USER=''
+PASSWD=''
+HOST=''
+BACKDIR=''
+TIME="$(date +"%Y%d%m")"
+MYSQL='/usr/bin/mysql'
+MYSQLDUMP='/usr/bin/mysqldump'
+GZIP='/bin/gzip'
+alldb=`$MYSQL -u $USER -p$PASSWD -h $HOST -Bse "show databases"|grep -v "information_schema"|grep -v "mysql"|grep -v "test"`
+for db in $alldb;do
+    $MYSQLDUMP -u $USER -p$PASSWD $db|$GZIP -9 >$BACKDIR/$TIME-$i.gz;
 done
 #-------------------I am boring line------------------------------------
