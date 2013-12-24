622ee8075ed6abb4847411699987b2bcdd4593be
wocin <925511765@qq.com>
2013-12-24 11:16:24 +0800

    Update apache-iphits.py

--- apache-iphits.py
+++ apache-iphits.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 #By wocin
 #Email ---
+#analysis nginx ip hits or apache ip hits
 #-------------------I am boring line------------------------------------
-def calApacheIpHits(logfile):
-    iphits={}
+import sys
+def hits(logfile):
+    hits={}
     contents=open(logfile,'r')
     for line in contents:
         ip=line.split(" ",1)[0]
-        if 6<ip<=15:
-            iphits[ip]=iphits.get(ip,0)+1
-    return iphits
+        hits[ip]=hits.get(ip,0)+1
+    return hits
+logfile=sys.argv[1]
+print hits(logfile)
 #-------------------I am boring line------------------------------------
