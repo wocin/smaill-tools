828dac375ba56876dd7e8a4c9c166ec0e40dded0
wocin <925511765@qq.com>
2013-12-24 17:06:25 +0800

    Update bak-data.sh

--- bak-data.sh
+++ bak-data.sh
@@ -2,3 +2,8 @@
 #By wocin
 #Email ---
 #-------------------I am boring line------------------------------------
+#bakup mysql
+database=`mysql -u root -p"" -e "show databases" |grep -v "information_schema" |grep -v "mysql" |grep -v "test"|grep -v "Database"`
+for i in $database;do
+    mysqldump -u root -p"" $i>/backup/$i.sql;
+done
